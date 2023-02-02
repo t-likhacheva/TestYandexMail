b840d09a7e98cec61d235fcf75422286ec45c2ba
t-likhacheva <iofinat@gmail.com>
2023-02-02 15:05:17 +0400

    добавлен метод наличие текста на странице

--- src/test/java/org/example/help_methods/BaseMethodsSelenium.java
+++ src/test/java/org/example/help_methods/BaseMethodsSelenium.java
@@ -4,10 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.awt.*;
-import java.awt.event.KeyEvent;
 
 @Slf4j
 public class BaseMethodsSelenium extends WebDriverSettings {
@@ -23,28 +19,34 @@ public class BaseMethodsSelenium extends WebDriverSettings {
 
     }
 
+    public Boolean checkText(String text) {
+        return driver.findElement(By.linkText(text)).isDisplayed();
+    }
 
     public void clickElement(WebElement elementToClick) throws Exception {
         Thread.sleep(200);
         elementToClick.click();
         Thread.sleep(200);
     }
+
     public void clickElementJavaScript(WebElement elementToClick) throws Exception {
         Thread.sleep(200);
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
-        executor.executeScript("arguments[0].click();",elementToClick);
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", elementToClick);
         Thread.sleep(200);
     }
+
     public void actionClickElement(WebElement elementToClick) throws Exception {
         Actions actions = new Actions(driver);
         Thread.sleep(200);
         actions.click(elementToClick).build().perform();
         Thread.sleep(200);
     }
+
     public void inputText(String textToinput, WebElement inputField) throws Exception {
-       for(int i=0;i<20;i++){
-           inputField.sendKeys(Keys.BACK_SPACE);
-       }
+        for (int i = 0; i < 20; i++) {
+            inputField.sendKeys(Keys.BACK_SPACE);
+        }
         Thread.sleep(2000);
         inputField.click();
         inputField.sendKeys(textToinput);
