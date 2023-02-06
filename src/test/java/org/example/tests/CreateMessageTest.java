2c728e7288e885b5c8ece0d4f126ce2ef8a5930b
t-likhacheva <iofinat@gmail.com>
2023-02-06 17:48:41 +0400

    create i messsages

--- src/test/java/org/example/tests/CreateMessageTest.java
+++ src/test/java/org/example/tests/CreateMessageTest.java
@@ -5,22 +5,25 @@ import org.example.help_methods.WebDriverSettings;
 import org.example.models.Message;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 import static org.example.methods_front.drafts.DraftsMethods.checkInDrafts;
 import static org.example.methods_front.drafts.DraftsMethods.findAndOpenDraft;
 import static org.example.methods_front.inbox.InboxMethods.*;
 import static org.example.methods_front.login.LoginGroupMethods.login;
 import static org.example.methods_front.login.LoginGroupMethods.logout;
-import static org.example.methods_front.newMessage.OpenedMessageMethods.*;
+import static org.example.methods_front.newMessage.OpenedMessageMethods.closeMessage;
+import static org.example.methods_front.newMessage.OpenedMessageMethods.fillMessage;
 import static org.example.models.Message.fillObjectRandomParameters;
 
 
 public class CreateMessageTest extends WebDriverSettings {
-    public   Message message ;
-
+    public Message message;
+    public Integer iter = 3;
 
     @Test(description = "Проверка создания сообщения и сохранения в черновиках»", groups = {"Base"}, priority = 100)
-    public void testCreateMessage()  {
-        message= fillObjectRandomParameters();
+    public void testCreateMessage() {
+        message = fillObjectRandomParameters();
         login();
         openNewMail();
         fillMessage(message);
@@ -29,26 +32,31 @@ public class CreateMessageTest extends WebDriverSettings {
         goToDrafts();
         checkInDrafts(message);
         findAndOpenDraft(message);
-       // compareMessageParams(message);
-        closeMessage();
-        logout();
-    }
-    @Test(description = "Проверка создания 5 сообщений, проставление флага важности 2 письмам, частичное удаление  писем (важность, чекбокс по совпадению)»", groups = {"Base"}, priority = 90)
-    public void testComplex()  {
-//        message= fillObjectRandomParameters();
-        login();
-        openNewMail();
-//        fillMessage(message);
-//        closeMessage();
-//        goToInput();
-//        goToDrafts();
-//        checkInDrafts(message);
-//        findAndOpenDraft(message);
         // compareMessageParams(message);
         closeMessage();
         logout();
     }
 
+    @Test(description = "Проверка создания 5 сообщений, проставление флага важности 2 письмам, частичное удаление  писем (важность, чекбокс по совпадению)»", groups = {"Base"}, priority = 90)
+    public void testComplex() {
+        login();
+        ArrayList<Message> mesList = new ArrayList<Message>(iter);
+        for (int i = 0; i < iter; i++) {
+            Message mes = fillObjectRandomParameters();
+            mesList.add(mes);
+            openNewMail();
+            fillMessage(mes);
+            closeMessage();
+        }
+        goToInput();
+        goToDrafts();
+        for (int i = 0; i < iter; i++) {
+            if (i==2||i==3)
+            findAndOpenDraft(message);
+        }
+
+        logout();
+    }
 
 
 }
